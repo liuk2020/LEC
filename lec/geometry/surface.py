b34452866a59963e5c0b77a6c151b7bd6c222eb6
liuk2020 <lk2020@mail.ustc.edu.cn>
2023-08-22 21:48:26 +0800

    feat: geometry-surface

---
+++ lec/geometry/surface.py
@@ -0,0 +1,27 @@
+from typing import List
+from ..toroidalField import ToroidalField
+from ..toroidalField import derivatePol, derivateTor 
+from ..toroidalField import multiply
+
+
+class Surface:
+
+    def __init__(self, rField: ToroidalField, zField: ToroidalField) -> None:
+        self.r = rField
+        self.z = zField
+        self.dRdTheta = derivatePol(rField)
+        self.dRdPhi = derivateTor(rField)
+        self.dZdTheta = derivatePol(zField)
+        self.dZdPhi = derivateTor(zField)
+
+    @property
+    def mertic(self) -> List[List[ToroidalField]]:
+        g_thetatheta = multiply(self.dRdTheta, self.dRdTheta) + multiply(self.dZdTheta, self.dZdTheta)
+        g_thetaphi = multiply(self.dRdTheta, self.dRdPhi) + multiply(self.dZdTheta, self.dZdPhi)
+        g_phiphi = multiply(self.dRdPhi, self.dRdPhi) + multiply(self.r, self.r) + multiply(self.dZdPhi, self.dZdPhi)
+        return [[g_thetatheta, g_thetaphi], [g_thetaphi, g_phiphi]]
+
+
+
+if __name__ == "__main__":
+    pass
